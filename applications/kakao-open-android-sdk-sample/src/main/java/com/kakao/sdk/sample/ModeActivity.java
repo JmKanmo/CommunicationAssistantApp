af019a117de420636dea46ac23bac08b2f8b5dd6
JmKanmo <48584943+JmKanmo@users.noreply.github.com>
2019-10-24 11:46:02 +0900

    Update ModeActivity.java

--- applications/kakao-open-android-sdk-sample/src/main/java/com/kakao/sdk/sample/ModeActivity.java
+++ applications/kakao-open-android-sdk-sample/src/main/java/com/kakao/sdk/sample/ModeActivity.java
@@ -26,58 +26,31 @@ public class ModeActivity extends BaseActivity {
     private ImageView mode1;
     private ImageView mode2;
     private ImageView mode3;
-    // private ImageView logout;
     private String chat_user_name = FirebaseAuth.getInstance().getCurrentUser().getEmail().split("@")[0];
     private DatabaseReference reference1 = FirebaseDatabase.getInstance().getReference(chat_user_name).child("log");
     private boolean isdevice = false;
 
-
     protected void onCreate(Bundle savedInstanceState) {
-        Log.d("프로세스", "모드엑티비티실행");
         FindAddressClick(chat_user_name);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_modeselect);
-
         mRequestQue = Volley.newRequestQueue(this);
-        //device = findViewById(R.id.device);
         mode1 = findViewById(R.id.mode1);
         mode2 = findViewById(R.id.mode2);
         mode3 = findViewById(R.id.mode3);
 
         FirebaseMessaging.getInstance().subscribeToTopic(chat_user_name);
 
-        //logout = findViewById(R.id.logout_button);
-
-        /*
-        device.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), ChatActivity_device.class);
-                startActivity(intent);
-            }
-        });
-        */
-
+        /*모드1(일상소통) 뷰 전환*/
         mode1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //       ChatData chatData = new ChatData();
-                //     String temp = FirebaseDatabase.getInstance().getReference().push().toString();
-                //    DatabaseReference ref = reference1.child(temp.split("/")[temp.split("/").length - 1]);
-                //   chatData.setMsg("");
-                //  chatData.setFuntcion("일상생활");
-                // chatData.setUser_name(chat_user_name);
-                //chatData.setUser_phoneNumber(MainActivity.PhoneNumber);
-                // chatData.setUser_room("");
-                //chatData.setDatabaseReference(temp);
-                // ref.setValue(chatData);
-                //if (isdevice == false) {
-                //   ref.removeValue();
-                //}
                 Intent intent = new Intent(getApplicationContext(), ChatActivity_mode1.class);
                 startActivity(intent);
             }
         });
+        
+        /*모드2(학습) 뷰 전환*/
         mode2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -86,6 +59,7 @@ public class ModeActivity extends BaseActivity {
             }
         });
 
+        /*모드3(통화) 뷰 전환*/
         mode3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -94,98 +68,55 @@ public class ModeActivity extends BaseActivity {
                 String[] friendType = {FriendsRequest.FriendType.KAKAO_TALK.name()};
                 intent.putExtra(FriendsMainActivity.EXTRA_KEY_SERVICE_TYPE, friendType);
                 startActivity(intent);
-
-
-                //  Intent intent = new Intent(getApplicationContext(), RoomActivity_dial.class);
-                //   intent.putExtra("device", isdevice);
-                //  startActivity(intent);
             }
         });
-
-        /*
-        logout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                FirebaseAuth.getInstance().signOut();
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        });
-        */
     }
 
     @Override
     protected void onStart() {
-        Log.d("TAG_ModeActivity", "mode_onStart");
         if (getIntent().getExtras() != null) {
             if (getIntent().getExtras().get("device") != null) {
                 isdevice = (boolean) getIntent().getExtras().get("device");
-                if (isdevice) {
-                    Log.d("TAG_ModeActivity", "디바이스가 있습니다.");
-                    // device.setEnabled(false);
-                } else {
-                    Log.d("TAG_ModeActivity", "디바이스가 없습니다.");
-                }
             }
         }
         super.onStart();
     }
 
+    /*유저정보를 split하여 저장*/
     private String FindAddressClick(String id) {
-
         final String[] info = new String[1];
-
-
+        
         FirebaseDatabase.getInstance().getReference("유저목록").orderByPriority().addListenerForSingleValueEvent(new ValueEventListener() {
-
             String temp;
             String deaf;
             String nickname;
             String userId;
             String phoneNumber;
 
-
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-
-                Log.d("TTTT", dataSnapshot.getValue().toString());
-
-
                 for (String str : dataSnapshot.getValue().toString().split(",")) {
-
-
                     if (str.split("=")[0].equals(" msg")) {
                         deaf = str.split("=")[1];
                     }
-
                     if (str.split("=")[0].equals(" user_room")) {
                         nickname = str.split("=")[1];
                     }
-
                     if (str.split("=")[0].equals(" user_name")) {
                         userId = str.split("=")[1];
                     }
-
                     if (str.split("=")[0].equals(" user_phoneNumber")) {
                         phoneNumber = str.split("=")[1];
                     }
-
                     if (str.split("=")[0].equals(" function")) {
-
-                        // Log.d("프로세스", "여기 잘 돌아가나요? == > " + userId);
-                        //  Log.d("프로세스", "여기 잘 돌아가나요? == > " + id);
-
                         if (userId.equals(id)) {
                             temp = nickname + "&" + phoneNumber + "&" + deaf;
                             info[0] = temp;
                         }
-
                         deaf = "";
                         nickname = "";
                         userId = "";
                         phoneNumber = "";
-
                     }
                 }
             }
@@ -194,7 +125,6 @@ public class ModeActivity extends BaseActivity {
             public void onCancelled(@NonNull DatabaseError databaseError) {
             }
         });
-
         return info[0];
     }
 }
